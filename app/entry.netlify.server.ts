cab9c73963470510a9020b7283eb509e1a1540b3
Filiph Siitam Sandström <filfat@hotmail.se>
2025-12-11 15:53:37 +0100

    chore: Fix missing `setImmediate`, `clearImmediate`, and `Buffer`.

--- app/entry.netlify.server.ts
+++ app/entry.netlify.server.ts
@@ -5,6 +5,28 @@
  * It imports the unified server logic and exposes it as a default function export
  * which is what Netlify expects.
  */
+import { Buffer } from "node:buffer";
+
+if (!globalThis.Buffer) {
+    globalThis.Buffer = Buffer;
+}
+
+if (!globalThis.setImmediate) {
+    // @ts-expect-error - Typescript might complain about window/global mismatch
+    globalThis.setImmediate = (
+        // biome-ignore lint/nursery/noShadow: need to use args.
+        callback: (...args: any[]) => void,
+        ...args: any[]
+    ) => {
+        return setTimeout(callback, 0, ...args);
+    };
+}
+if (!globalThis.clearImmediate) {
+    globalThis.clearImmediate = (id: any) => {
+        clearTimeout(id);
+    };
+}
+
 import { buildEnvFromNetlify } from "~/lib/env.server";
 import server from "../server";
 
